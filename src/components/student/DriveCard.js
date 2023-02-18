dc5b3ebf44b8c6996f210f47bebc96e389429346
Anushka Kamalja <anushkakamalja@gmail.com>
2023-02-18 16:12:07 +0530

    Drive Card final design
    Signed-off-by: Anushka Kamalja <anushkakamalja@gmail.com>

--- src/components/student/DriveCard.js
+++ src/components/student/DriveCard.js
@@ -67,26 +67,6 @@ export default function DriveCard() {
           </div>
         </div>
       </div>
-      // <div class=" mt-10   max-w">
-      //   <div onClick={handleDriveCardClick} class="hover:scale-105 hover:cursor-grab duration-500 flex flex-col ml-40 mr-40 h-40 md:flex-row md:max-w-4xl rounded-lg bg-white shadow-lg group block max-w-xs mx-auto r  hover:bg-[#edecec] ">
-      //     <img
-      //       class=" w-full h-96 md:h-auto object-cover md:w-48 rounded-t-lg md:rounded-none md:rounded-l-lg"
-      //       src="https://mdbootstrap.com/wp-content/uploads/2020/06/vertical.jpg"
-      //       alt=""
-      //     />
-      //     <div class="p-6 flex flex-col justify-start w-100">
-      //       <h5 class="text-gray-900 text-xl font-medium mb-2">TCS DIGITAL</h5>
-      //       <p class="text-gray-700 text-base mb-3">
-      //         All TCS aspirants who would eventually be hired through TCS Digital will work in areas like Internet of Things, Big Data & Analytics, Cloud Computing, Artificial Intelligence, etc.
-      //       </p>
-
-
-
-
-      //     </div>
-      //   </div>
-
-      // </div>
       
     
   );
