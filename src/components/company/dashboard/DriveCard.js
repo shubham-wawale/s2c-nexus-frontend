9cf7f7573909867b2ac3e58f2e9f3ae7c2403505
ashutoshkarwa18 <69465593+ashutoshkarwa18@users.noreply.github.com>
2023-02-18 23:08:23 +0530

    Added Company Signup Form

--- src/components/company/dashboard/DriveCard.js
+++ src/components/company/dashboard/DriveCard.js
@@ -60,7 +60,7 @@ export default function DriveCard(props) {
             <div class="flex flex-row flex-right  justify-between ">
             {/* <button
             type="button"
-            class="ml-80 inline-block px-6 py-3 bg-[#0f172a] text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-gradient-to-r from-indigo-700 via-purple-700 to-pink-700 ... hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
+            class="ml-80 px-6 py-3 bg-[#0f172a] text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-gradient-to-r from-indigo-700 via-purple-700 to-pink-700 ... hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
           >
             View Details
           </button> */}
