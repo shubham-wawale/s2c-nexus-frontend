3a93bfa87559aa81034830ea80ec59b327e56c77
shubham-wawale <wawaleshubham@gmail.com>
2023-02-19 21:09:23 +0530

    Added automatic field filling in project details

--- src/components/student/Forms/ExperienceDetails.js
+++ src/components/student/Forms/ExperienceDetails.js
@@ -1,70 +1,126 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
 
 export default function ExperienceDetails() {
+  useEffect(() => {
+    axios.get("http://localhost:8080/student/getDetails", {
+      params: {
+        studentId: "638d14885c37719538d6c86d"
+      }
+    }).then(response => {
+      var data = response.data.studentData;
+      if (data.experienceDetails) {
+        setExperienceData(data.experienceDetails)
+      }
+    }).catch(error => {
+      console.log(error)
+    })
+  }, [])
+  const [experienceData, setExperienceData] = useState([])
+  const [experienceDetails, setExperienceDetails] = useState({
+    position: "",
+    duration: "",
+    location:  "",
+    jobType: "Internship",
+    organisation: "",
+    workDescription: "",
+    startDate: "",
+    endDate: ""
+  })
+
+  const handleInputChange = e => {
+    const {name,value} = e.target
+    setExperienceDetails(prevValue => ({
+      ...prevValue,
+      [name]: value
+    }))
+  }
+
+  const handleAddExperience = e => {
+    e.preventDefault()
+    setExperienceData(prevValue => (
+      [
+        ...prevValue,
+        experienceDetails
+      ]
+    ))
+  }
+
+  const handleExperienceDetailsUpdate = e => {
+    e.preventDefault()
+
+  }
+
   return (
     <>
     <div className='font-open-sans overflow-scroll scrollbar-hide h-105 mx-4 shadow-md border-b mb-5 rounded-md'>
     <div className="left-0 my-2 uppercase focus:outline-none border-none 
         bg-[#C2D3E4] border font-bold text-[#1F2937] text-xs  py-1 px-2 ">
       <span>Enter Your Professional Experience</span></div>
+      <div className='mx-5 mt-2 grid grid-cols-2 gap-4'>
+        {experienceData.map(experience=> (
+          <div class="border cols-span-2 p-2 bg-[#C2D3E4]">
+          <b>Position: {experience.position}</b>
+        </div>
+        ))}
+      </div>
     <div className='mx-5 mt-2 grid grid-cols-2 gap-4'>
       <div class="mb-0">
-        <label for="tenthPassingYear" class="block  text-sm font-bold text-[#1F2937]">Company Name</label>
-        <input name='tenthPassingYear' type="text" class="shadow-sm focus:bg-[#C2D3E4] focus:outline-none 
+        <label for="organisation" class="block  text-sm font-bold text-[#1F2937]">Company Name</label>
+        <input onChange={handleInputChange} name='organisation' type="text" class="shadow-sm focus:bg-[#C2D3E4] focus:outline-none 
       p-1.5  w-full   bg-gray-50 border border-gray-300 text-[#1F2937]  text-sm rounded-lg" />
       </div>
       <div class="mb-0">
-        <label for="tenthPassingYear" class="block  text-sm font-bold text-[#1F2937]">Job Role</label>
-        <input name='tenthPassingYear' type="text" class="shadow-sm focus:bg-[#C2D3E4] focus:outline-none 
+        <label for="position" class="block  text-sm font-bold text-[#1F2937]">Job Role</label>
+        <input onChange={handleInputChange} name='position' type="text" class="shadow-sm focus:bg-[#C2D3E4] focus:outline-none 
       p-1.5  w-full   bg-gray-50 border border-gray-300 text-[#1F2937]  text-sm rounded-lg" />
       </div>
   
       <div class=" cols-span-1">
-            <label for="semester" class="block  text-sm font-bold text-[#1F2937]">Job Type</label>
-            <select name="semester" class="shadow-sm focus:bg-[#C2D3E4] focus:outline-none 
+            <label for="jobType" class="block  text-sm font-bold text-[#1F2937]">Job Type</label>
+            <select onChange={handleInputChange} name="jobType" class="shadow-sm focus:bg-[#C2D3E4] focus:outline-none 
           p-1.5  w-full   bg-gray-50 border border-gray-300 text-[#1F2937]  text-sm rounded-lg">
-              <option value="1">Internship</option>
-              <option value="2">Full Time</option>
+              <option value="Internship">Internship</option>
+              <option value="Full-Time">Full Time</option>
             </select>
           </div>
           <div class=" cols-span-1">
-            <label for="semesterGpa" class="block  text-sm font-bold text-[#1F2937]">Job Location</label>
-            <input name='semesterGpa' type="text" class=" cursor-pointer shadow-sm focus:bg-[#C2D3E4] focus:outline-none 
+            <label for="location" class="block  text-sm font-bold text-[#1F2937]">Job Location</label>
+            <input onChange={handleInputChange} name='location' type="text" class=" cursor-pointer shadow-sm focus:bg-[#C2D3E4] focus:outline-none 
           p-1.5  w-full   bg-gray-50 border border-gray-300 text-[#1F2937]  text-sm rounded-lg" />
           </div>
           
           
       <div class="mb-3 col-span-2">
-          <label for="address" class="block  text-sm font-bold text-[#1F2937]">Your Role Details</label>
-          <textarea name='address' type="text" id="address" class="shadow-sm focus:bg-[#C2D3E4] focus:outline-none 
+          <label for="workDescription" class="block  text-sm font-bold text-[#1F2937]">Your Role Details</label>
+          <textarea onChange={handleInputChange} name='workDescription' type="text" id="address" class="shadow-sm focus:bg-[#C2D3E4] focus:outline-none 
           p-2.5 w-full bg-gray-50 border border-gray-300 text-[#1F2937]  text-sm rounded-lg" />
         </div>
     </div>
     <div className='mx-5 mt-0 grid grid-cols-3 gap-4'>
     <div class="mb-3 cols-span-1">
-            <label for="degreeStartDate" class="block text-sm font-bold text-[#1F2937]">Start Date</label>
-            <input name='degreeStartDate' datepicker type="date" class="shadow-sm focus:bg-[#C2D3E4] focus:outline-none 
+            <label for="startDate" class="block text-sm font-bold text-[#1F2937]">Start Date</label>
+            <input onChange={handleInputChange} name='startDate' datepicker type="date" class="shadow-sm focus:bg-[#C2D3E4] focus:outline-none 
           p-1.5  w-full   bg-gray-50 border border-gray-300 text-[#1F2937]  text-sm rounded-lg" />
           </div>
           <div class="mb-3 cols-span-1">
-            <label for="degreeCompletionDate" class="block text-sm font-bold text-[#1F2937]">End Date</label>
-            <input name='degreeCompletionDate' datepicker type="date" class="shadow-sm focus:bg-[#C2D3E4] focus:outline-none 
+            <label for="endDate" class="block text-sm font-bold text-[#1F2937]">End Date</label>
+            <input onChange={handleInputChange} name='endDate' datepicker type="date" class="shadow-sm focus:bg-[#C2D3E4] focus:outline-none 
           p-1.5  w-full   bg-gray-50 border border-gray-300 text-[#1F2937]  text-sm rounded-lg" />
           </div>
           <div class="mb-3 cols-span-1">
-            <label for="degreeName" class="block text-sm font-bold text-[#1F2937]">Experience in Months</label>
-            <input name='degreeName' type="text" class="shadow-sm focus:bg-[#C2D3E4] focus:outline-none 
+            <label for="duration" class="block text-sm font-bold text-[#1F2937]">Experience in Months</label>
+            <input onChange={handleInputChange} name='duration' type="text" class="shadow-sm focus:bg-[#C2D3E4] focus:outline-none 
           p-1.5  w-full   bg-gray-50 border border-gray-300 text-[#1F2937]  text-sm rounded-lg" />
           </div>
-          
+          <div class="mb-3 cols-span-1">
+            <button onClick={handleAddExperience} className="mt-4 w-1/2 focus:outline-none bg-[#1F2937] px-1 py-1 rounded-md font-bold text-lg text-[#d1d5db] border">Add Experience</button>
+          </div>
         </div>
-        <div class="mb-3 cols-span-1">
-            <button className="mt-4 w-1/5 focus:outline-none bg-[#1F2937] px-1 py-1 rounded-md font-bold text-lg text-[#d1d5db] border">Add Experience</button>
-          </div>
     </div>
     
     
-  <button className="float-right mr-4 ml-auto focus:outline-none bg-[#1F2937] px-1 py-1 rounded-md font-bold text-lg text-[#d1d5db] w-28 border">Save</button>
+  <button onClick={handleExperienceDetailsUpdate} className="float-right mr-4 ml-auto focus:outline-none bg-[#1F2937] px-1 py-1 rounded-md font-bold text-lg text-[#d1d5db] w-28 border">Save</button>
 </>
   )
 }
--- src/components/student/Forms/ProjectDetails.js
+++ src/components/student/Forms/ProjectDetails.js
@@ -1,32 +1,100 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios';
 
 
 export default function ProjectDetails() {
+  useEffect(() => {
+    axios.get("http://localhost:8080/student/getDetails", {
+      params: {
+        studentId: "638d14885c37719538d6c86d"
+      }
+    }).then(response => {
+      var data = response.data.studentData;
+      if (data.projectDetails) {
+        setProjectData(data.projectDetails)
+      }
+    }).catch(error => {
+      console.log(error)
+    })
+  }, [])
+
+  const [projectData, setProjectData] = useState([])
+  const [projectDetails, setProjectDetails] = useState({
+    name: "",
+    description: ""
+  })
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target
+    setProjectDetails(prevValue => ({
+      ...prevValue,
+      [name]: value
+    }))
+  }
+
+  const handleAddProject = (e) => {
+    e.preventDefault()
+    setProjectData(prevValue => (
+      [
+        ...prevValue,
+        projectDetails
+      ]
+    ))
+  }
+
+  const handleProjectDetailsUpdate = (e) => {
+    e.preventDefault()
+    axios.post('http://localhost:8080/student/updateDetails', {
+      studentId: "638d14885c37719538d6c86d",
+      key: "projectDetails",
+      newData: projectData
+    })
+    .then(function (response) {
+      if(response.data.success) {
+        alert(response.data.message)
+      } else {
+        alert(response.data.message)
+      }
+    })
+    .catch(function (error) {
+      alert("BACKEND SERVICE UNAVAILABLE");
+    })
+  }
   return (
     <>
-    <div className='font-open-sans overflow-scroll scrollbar-hide h-105 mx-4 shadow-md border-b mb-5 rounded-md'>
-    <div className="left-0 my-2 uppercase focus:outline-none border-none 
+      <div className='font-open-sans overflow-scroll scrollbar-hide h-105 mx-4 shadow-md border-b mb-5 rounded-md'>
+        <div className="left-0 my-2 uppercase focus:outline-none border-none 
         bg-[#C2D3E4] border font-bold text-[#1F2937] text-xs  py-1 px-2 ">
-      <span>Enter Your Project Details</span></div>
-    <div className='mx-5 mt-2 grid grid-cols-2 gap-4'>
-      <div class="mb-0">
-        <label for="tenthPassingYear" class="block  text-sm font-bold text-[#1F2937]">Project Name</label>
-        <input name='tenthPassingYear' type="text" class="shadow-sm focus:bg-[#C2D3E4] focus:outline-none 
+          <span>Enter Your Project Details</span></div>
+          {projectData.map(project => (
+            <div className='mx-5 mt-2 grid grid-cols-2 gap-4'>
+            <div class="mb-3 cols-span-1">
+              <b>Project Name:</b> {project.name}
+            </div>
+            <div class="mb-3 cols-span-1">
+              <b>Project Description:</b> {project.description}
+            </div>
+            </div>
+          ))}
+        <div className='mx-5 mt-2 grid grid-cols-2 gap-4'>
+          <div class="mb-0">
+            <label for="name" class="block  text-sm font-bold text-[#1F2937]">Project Name</label>
+            <input onChange={handleInputChange} name='name' type="text" class="shadow-sm focus:bg-[#C2D3E4] focus:outline-none 
       p-1.5  w-full   bg-gray-50 border border-gray-300 text-[#1F2937]  text-sm rounded-lg" />
-      </div>
-      <div class="mb-3 col-span-2">
-          <label for="address" class="block  text-sm font-bold text-[#1F2937]">Project Details</label>
-          <textarea name='address' type="text" id="address" class="shadow-sm focus:bg-[#C2D3E4] focus:outline-none 
-          p-2.5 w-full bg-gray-50 border border-gray-300 text-[#1F2937]  text-sm rounded-lg" />
-        </div>
-    </div>
-    <div class="mb-3 cols-span-1">
-            <button className="mt-2 w-1/5 focus:outline-none bg-[#1F2937] px-1 py-1 rounded-md font-bold text-lg text-[#d1d5db] border">Add Project</button>
           </div>
-    </div>
-    
-  <button className="float-right mr-4 ml-auto focus:outline-none bg-[#1F2937] px-1 py-1 rounded-md font-bold text-lg text-[#d1d5db] w-28 border">Save</button>
-</>
+          <div class="mb-3 col-span-2">
+            <label for="description" class="block  text-sm font-bold text-[#1F2937]">Project Details</label>
+            <textarea onChange={handleInputChange} name='description' type="text" id="address" class="shadow-sm focus:bg-[#C2D3E4] focus:outline-none 
+          p-2.5 w-full bg-gray-50 border border-gray-300 text-[#1F2937]  text-sm rounded-lg" />
+          </div>
+          <div class="mb-3 cols-span-1">
+            <button onClick={handleAddProject} className="mt-2 w-1/2 focus:outline-none bg-[#1F2937] px-1 py-1 rounded-md font-bold text-lg text-[#d1d5db] border">Add Project</button>
+          </div>
+        </div>
+      </div>
+
+      <button onClick={handleProjectDetailsUpdate} className="float-right mr-4 ml-auto focus:outline-none bg-[#1F2937] px-1 py-1 rounded-md font-bold text-lg text-[#d1d5db] w-28 border">Save</button>
+    </>
 
   )
 }
